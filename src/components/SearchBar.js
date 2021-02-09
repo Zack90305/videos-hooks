5ca298e518dd42e5f03dc5cb354427e122340514
Zack90305 <zwashdev@gmail.com>
2021-02-09 14:23:27 +0000

    first commit. Code refactor from class component of SearchBar to function component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class SearchBar extends React.Component {
-	state = { term: '' };
+//class SearchBar extends React.Component {
+//add destructured onFormSubmit arg to fx decl
+const SearchBar = ({ onFormSubmit }) => {
+	//state = { term: '' };
+	const [term, setTerm] = useState('');
 	
-	onInputChange = (event) => {
-		this.setState({ term: event.target.value });
+	//delete const to make class component
+	const onInputChange = (event) => {
+		//this.setState({ term: event.target.value }); makes no sense outside of a class component
+		setTerm(event.target.value);
 	};
 
-	onFormSubmit = (event) => {
+	//delete const to make class component
+	//name collision on function - renamed from onFormSubmit to onSubmit
+	const onSubmit = (event) => {
 		event.preventDefault();
-		
-		this.props.onFormSubmit(this.state.term);
+	
+		//this.props.onFormSubmit(this.state.term); makes no sense outside of a class component
+		onFormSubmit(term);
 	};
-
-	render() {
-		return (
-			<div className="search-bar ui segment">
-				<form onSubmit={this.onFormSubmit} className="ui form">
-					<div className="field">
-						<label>Video Search</label>
-						<input onChange={this.onInputChange} type="text" value={this.state.term}/>
-					</div>
-				</form>
-			</div>
-		);
-	}
-}
+	
+	//removed render() and added fx body to fx component
+	//this.onFormSubmit->onSubmit this.onInputChange->onInputChange this.state.term->term
+	return (
+		<div className="search-bar ui segment">
+			<form onSubmit={onSubmit} className="ui form">
+				<div className="field">
+					<label>Video Search</label>
+					<input onChange={onInputChange} type="text" value={term}/>
+				</div>
+			</form>
+		</div>
+	);
+};
 
 export default SearchBar;
